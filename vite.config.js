6eb67e2862876ca45fe4f75fc343d8b6e0d8b17d
oyis12 <oyivosam12@gmail.com>
2026-03-02 12:16:21 +0100

    updated now

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // // https://vite.dev/config/
 // export default defineConfig({
@@ -7,12 +7,15 @@
 // })
 
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
 
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: 'build',
+//   },
+// })
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
 })
